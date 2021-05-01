94900748c37468d3b2eb987c7436cc1a5a741123
Cuchi <42467470+Cu-chi@users.noreply.github.com>
2021-05-01 18:05:34 +0200

    Ajoutez des fichiers projet.

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DiscordRPC;
+
+namespace CustomDiscordRichPresence
+{
+    public partial class CustomRichPresence : Form
+    {
+        public DiscordRpcClient client;
+        public string applicationID = "";
+
+        public CustomRichPresence()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void DiscordRichPresence()
+        {
+            client = new DiscordRpcClient(applicationID);
+
+            client.Initialize();
+
+            client.SetPresence(new RichPresence()
+            {
+                Details = "Debugging RichPresence",
+                State = "Working...",
+                Assets = new Assets()
+                {
+                    LargeImageKey = "logo",
+                    LargeImageText = "RPC",
+                }
+            });
+        }
+    }
+}
